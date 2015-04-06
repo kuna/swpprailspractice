859fde465b07b2ef033ceec139ad3f998cf42d56
kuna <kgdgwn98@gmail.com>
2015-04-06 20:47:42 +0900

    done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,51 @@
 class ArticlesController < ApplicationController
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+        # redirect to '/articles/index'
+    end
+
     def new
+        @article = Article.new
     end
 
     def index
-        @article = Article.all
+        @articles = Article.all
     end
 
     def show
         @article = Article.find(params[:id])
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
     def create
         @article = Article.new(article_params)
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
         #render plain: params[:article].inspect
+    end
+
+    def count
+        num_of_articles = Article.all.count
+        render :json => { count: num_of_articles }
     end
 
     private
